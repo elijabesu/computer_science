9b49e075ccdc44bf0118fe12160e8be813128d32
Ellie <ellie@saurich.com>
2020-04-03 07:32:53 +0200

    Update exercise3.c

--- C/exercise3.c
+++ C/exercise3.c
@@ -1,6 +1,84 @@
 /* 
-   Soubor s testy k prvni domaci uloze ze ZP2 a YUP2
-*/
+   DOMACI UKOL 1
+
+   studenti ZP2: za ukol je mozne ziskat 15 bodu. Ukol je nutne odevzdat do
+   5. dubna 2020 (vcetne).
+
+   
+   studenti YUP2: za ukol je mozne ziskat 15 bodu. Ukol je nutne odevzdat do
+   20. srpna 2020 (vcetne).
+
+   Ukol odevzdavejte emailem (osicka@acm.org) s hlavickou ZP2 - domaci ukol 1,
+   vyreseny ukol prilozte jako prilohu mailu, do textu mailu napiste sve jmeno.
+
+   --------
+  | ZADANI |
+   --------
+   
+   Implementuje funkci 
+
+   char **string_split (char *text, char delimiter, int *size)
+
+   Tato funkce rozdeli vstupni retezec text na casti oddelene znakem
+   delimiter. Napriklad retezec
+
+   "ahoj svete" 
+   
+   rozdeli na dva retezce 
+   
+   "ahoj" a "svete".
+
+   Funkce vrati pole takto vzniklych NOVE ALOKOVANYCH retezcu a na adresu
+   size zapise jejich pocet. Tedy v pripade
+
+   int length = 0;
+   char **split = string_split("ahoj svete", ' ', &length);
+
+   funkce ulozi do promenne length hodnotu 2, dale pak
+   
+   split[0] bude obsahovat retezec "ahoj"
+   split[1] bude obsahovat retezec "svete"
+
+   pricemz split[0] a split[1] adresy retezcu, ktere 
+   byly alokovany mallocem a jejich obsah tam byl zkopirovan z 
+   retezce "ahoj svete".
+
+   Ve funkci je tedy potreba pracovat s dvourozmernym polem alokovanym
+   na heapu, podrobnosti k tomuto je nutno nastudovat napriklad
+   z tohoto zdroje:
+   
+   http://jazykc.inf.upol.cz/dynamicka-vicerozmerna-pole/index.htm
+
+   ------------------
+  | OKRAJOVE CHOVANI |
+   ------------------
+
+   1) pokud je text prazdny retezec, funkce vrati jednoprvkove pole,
+      jehoz prvnim prvkem bude prazdny retezec
+
+      Priklad:
+      "" -> {""}
+
+   2) pokud je delimiter na zacatku retezce, je prvnim prvkem vraceneho
+      pole prazdny retezec.
+
+      Priklad (delimiter a)
+      "axxx" -> {"", "xxx"}
+
+   3) pokud je delimiter na konci retezce, je poslednim prvkem vraceneho
+      pole prazdny retezec.
+      
+      Priklad (delimiter a)
+      "xxxa" -> {"xxx", ""}
+
+   4) pokud retezec (nebo jeho cast) obsahuje dva delimitery za sebou,
+      vede to k pridani prazdneho retezce na prislusne misto ve 
+      vyslednem poli.
+
+      Priklad (delimiter a)
+      "xxaaaxx" -> {"xx", "", "", "xx"}
+
+ */
 
 void _print_splits (char **splits, int l) {
 
